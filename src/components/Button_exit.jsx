189893ff904c947093ff63725e729093b333f0e6
P1n0 <pinjup_@pinjup.local>
2023-07-07 20:16:08 -0500

    Fix problem background and font color of button

--- src/components/Button_exit.jsx
+++ src/components/Button_exit.jsx
@@ -1,7 +1,7 @@
 export default function Button_exit({ size, bg_color, text, icon, text_color, children }) {
 
     return (
-        <button className={`button_exit ${size == 'normal' ? 'button_exit--normal' : 'button_exit--large'} ${bg_color} ${text_color}`}>
+        <button style={{ backgroundColor: bg_color, color: text_color }} className={`button_exit ${size == 'normal' ? 'button_exit--normal' : 'button_exit--large'}`}>
             <a>{text}</a>
             {icon ? (children) :(<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
--- src/components/Main_content.jsx
+++ src/components/Main_content.jsx
@@ -262,7 +262,7 @@ export default function Main_content() {
 
                     <p>Here, you will find an array of stunning pieces that will take your home decor <br /> to the next level. Our furniture combines aesthetic appeal with functionality <br /> to give you the best of both worlds.</p>
 
-                    <Button_exit size='large' bg_color='bg-[#fbd464]' text='Explore Products' icon={false} text_color='text-black' />
+                    <Button_exit size='large' bg_color='#fbd464' text='Explore Products' icon={false} text_color='black' />
 
                 </div>
 
@@ -304,7 +304,7 @@ export default function Main_content() {
                         <p className="text-white font-extralight text-center text-xs leading-5">Experience comfort and style with our handcrafted furniture <br /> discover the story behind our passionate mission.</p>
                     </div>
 
-                    <Button_exit size='normal' bg_color='bg-[#fbd464]' text='Learn More' icon={false} text_color='text-black' />
+                    <Button_exit size='normal' bg_color='#fbd464' text='Learn More' icon={false} text_color='black' />
                     
                 </div>
 
@@ -349,7 +349,7 @@ export default function Main_content() {
                             
                         </ul>
 
-                        <Button_exit size='normal' bg_color='bg-[#7aa668]' text='Learn More' icon={false} text_color='text-white' />
+                        <Button_exit size='normal' bg_color='#7aa668' text='Learn More' icon={false} text_color='white' />
 
                     </div>
 
@@ -437,7 +437,7 @@ export default function Main_content() {
                     </div>
 
                     <div className="button_container">
-                        <Button_exit size='large' bg_color='bg-[#7aa668]' text='See All Products' icon={false} text_color='text-white' />
+                        <Button_exit size='large' bg_color='#7aa668' text='See All Products' icon={false} text_color='white' />
                     </div>
                 </div>
 
@@ -519,7 +519,7 @@ export default function Main_content() {
                         
                         <input type="text" placeholder="Enter email address"  />
 
-                        <Button_exit size='normal' bg_color='bg-[#fbd464]' text='Subscribe' icon={true} text_color='text-black'>
+                        <Button_exit size='normal' bg_color='#fbd464' text='Subscribe' icon={true} text_color='black'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
                             </svg>
